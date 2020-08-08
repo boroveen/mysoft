3fbc04b8085ae59349d4d05996a538ecd4a2c242
boroveen <56752476+boroveen@users.noreply.github.com>
2020-08-08 20:34:10 +0300

    Create yk.cpp

---
+++ beta/yk.cpp
@@ -0,0 +1,28 @@
+#include <ncurses.h>
+using namespace std;
+int main(){
+initscr();
+noecho();
+curs_set(0);
+bool gg=false;
+int row,cal;
+start_color();
+init_pair(1,COLOR_YELLOW,COLOR_YELLOW);
+init_pair(2,COLOR_BLUE,COLOR_BLUE);
+while(!gg){
+getmaxyx(stdscr,row,cal);
+attron(COLOR_PAIR(1));
+for(int i=0; i<row/2*cal; i++) printw("h");
+
+attron(COLOR_PAIR(2));
+for(int i=0; i<row/2*cal; i++){
+printw("$");
+}
+switch(getch()){
+case 410: clear(); break;
+default: gg=true; break;
+}
+}
+endwin();
+return 0;
+}

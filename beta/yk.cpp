4794b976447e708f8ab7881dcd45e808892ef953
boroveen <56752476+boroveen@users.noreply.github.com>
2020-08-13 21:22:33 +0300

    Update yk.cpp

--- beta/yk.cpp
+++ beta/yk.cpp
@@ -11,10 +11,10 @@ init_pair(1,COLOR_YELLOW,COLOR_YELLOW);
 init_pair(2,COLOR_BLUE,COLOR_BLUE);
 while(!gg){
 getmaxyx(stdscr,row,cal);
-attron(COLOR_PAIR(1));
+attron(COLOR_PAIR(2));
 for(int i=0; i<row/2*cal; i++) printw("h");
 
-attron(COLOR_PAIR(2));
+attron(COLOR_PAIR(1));
 for(int i=0; i<row/2*cal; i++){
 printw("$");
 }

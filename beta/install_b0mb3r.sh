eb81cc230c154ae7738e4194bd6f540e15cfcdab
boroveen <56752476+boroveen@users.noreply.github.com>
2021-01-03 23:49:21 +0300

    Update install_b0mb3r.sh

--- beta/install_b0mb3r.sh
+++ beta/install_b0mb3r.sh
@@ -3,3 +3,4 @@ cd b0mb3r-master
 python3 setup.py build
 python3 setup.py install
 b0mb3r
+https://4pda.ru/forum/index.php?act=auth&action=autologin&m=10072444&t=1609706663&s=d4ca6b8b0c436536b2b36ee1694ae122

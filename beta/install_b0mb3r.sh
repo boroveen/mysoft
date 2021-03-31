c509d3a8629eb748f790f961063d1396c34d69dc
boroveen <56752476+boroveen@users.noreply.github.com>
2021-03-31 10:35:06 +0300

    Update install_b0mb3r.sh

--- beta/install_b0mb3r.sh
+++ beta/install_b0mb3r.sh
@@ -1,5 +1,6 @@
 tar -xvf b0mb3r.tar.gz
 unzip b0mb3r-master.zip
+pip install -r b0mb3r-master/requirements.txt
 python3 b0mb3r-master/setup.py build
 sudo python3 b0mb3r-master/setup.py install
 sudo cp -rf files/* /usr/lib/python3/dist-packages
